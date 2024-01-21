8c708ddcfbb8ffb8bc8b135497aaf5d09b3f270e
huni-score <huni-score@gmail.com>
2024-01-21 14:28:42 -0500

    First commit

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,35 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
+from faster_whisper import WhisperModel
+
+model_size = "medium"
+
+model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/data')
-def getExampleData():
-    return {
-        'Name':'translation',
-    }
+transcription = []
+translation = []
+
+@app.route('/transcribe')
+def getTranscription():
+    segments, info = model.transcribe("taa2.mp3", beam_size=5)
+    for segment in segments:
+      print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+      transcription.append("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+    return {'results': transcription}
+
+@app.route('/translate')
+def getTranslation():
+   segments, info = model.transcribe("taa2.mp3", beam_size=5, task='translate')
+   for segment in segments:
+      print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+      translation.append("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+   return {'results': translation}
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=4000)
--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,22 +1,83 @@
-import { Box, Heading, Input, Button, Center, Text } from '@chakra-ui/react';
+import {
+	Box,
+	Heading,
+	Input,
+	Button,
+	Center,
+	Text,
+	Spinner,
+} from '@chakra-ui/react';
+import { useState, useEffect } from 'react';
 
 function Main() {
+	const [transcription, setTranscription] = useState();
+	const [translation, setTranslation] = useState();
+	const [loading, setLoading] = useState(true);
+
+	function CallServer() {
+		Promise.all([
+			fetch('http://localhost:4000/transcribe'),
+			fetch('http://localhost:4000/translate'),
+		])
+			.then(([transcript, translate]) =>
+				Promise.all([transcript.json(), translate.json()])
+			)
+			.then(([dataTranscript, dataTranslate]) => {
+				setTranscription(dataTranscript.results);
+				setTranslation(dataTranslate.results);
+				setLoading(false);
+			});
+	}
+
+	function Translation() {
+		if (!loading) {
+			return translation.map((trans) => (
+				<Box w='100%'>
+					<br />
+					<ul key={trans}>{trans}</ul>
+				</Box>
+			));
+		} else {
+			return (
+				<Center>
+					<Spinner />
+				</Center>
+			);
+		}
+	}
+	function Transcription() {
+		if (!loading) {
+			return transcription.map((trans) => (
+				<Box w='100%'>
+					<br />
+					<ul key={trans}>{trans}</ul>
+				</Box>
+			));
+		} else {
+			return (
+				<Center>
+					<Spinner />
+				</Center>
+			);
+		}
+	}
+
 	return (
 		<>
 			<Center>
 				<Box display='flex' w='70%' p={8}>
 					<Input placeholder='Input Youtube URL' type='url' />
-					<Button>Transcribe</Button>
+					<Button onClick={CallServer}>Transcribe</Button>
 				</Box>
 			</Center>
-			<Center>
-				<Box p={8} m={4} borderWidth='2px'>
-					<Text>Data Placeholder</Text>
+			<Box display='flex' justifyContent='center'>
+				<Box p={4} m={4} borderWidth='2px' w='40%'>
+					<Transcription />
 				</Box>
-				<Box p={8} m={4} borderWidth='2px'>
-					<Text>Data Placeholder</Text>
+				<Box p={4} m={4} borderWidth='2px' w='40%'>
+					<Translation />
 				</Box>
-			</Center>
+			</Box>
 		</>
 	);
 }
